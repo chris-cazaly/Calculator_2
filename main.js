37beebd7a5980cef0935cd925b24d70b45496828
Chris Cazaly <chris.cazaly@coop.co.uk>
2021-11-05 13:16:45 +0000

    DOM Selectors

---
+++ main.js
@@ -0,0 +1,11 @@
+// DOM Selectors
+
+const numberButtons = document.querySelectorAll(".number-button");
+const mathOperatorButtons = document.querySelectorAll(".math-operator");
+const specialOperatorButtons = document.querySelectorAll(".special-operator");
+
+const num1 = document.querySelector(".num1-text");
+const operator = document.querySelector(".operator-text");
+const num2 = document.querySelector(".num2-text");
+
+
