fcacdcc8fddae89a6ad6160092f3307661bcb582
Chris Cazaly <chris.cazaly@coop.co.uk>
2021-11-05 13:28:26 +0000

    Add event listeners (numbers, math operators)

--- main.js
+++ main.js
@@ -8,4 +8,27 @@ const num1 = document.querySelector(".num1-text");
 const operator = document.querySelector(".operator-text");
 const num2 = document.querySelector(".num2-text");
 
+isNum1 = true;
 
+// Event listeners
+
+// Numbers
+for (let i = 0; i < numberButtons.length; i++) {
+  const thisButton = numberButtons[i];
+
+  thisButton.addEventListener("click", () => {
+    if (isNum1) {
+      num1.innerText += thisButton.innerText;
+    }
+  });
+}
+
+// Math operators
+for (let i = 0; i < mathOperatorButtons.length; i++) {
+  const thisButton = mathOperatorButtons[i];
+  console.log(thisButton.innerText);
+
+  thisButton.addEventListener("click", () => {
+    operator.innerText = thisButton.innerText;
+  });
+}
